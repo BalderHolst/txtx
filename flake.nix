1800dafe5b5be141c087134ab144fcdb8b8a1400
BalderHolst <balderwh@gmail.com>
2024-10-30 06:29:20 +0100

    better flake

--- flake.nix
+++ flake.nix
@@ -4,10 +4,20 @@
     inputs.flake-utils.url = "github:numtide/flake-utils";
 
     outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.simpleFlake {
-            inherit self nixpkgs;
-            name = "txtx";
-            overlay = ./overlay.nix;
-            shell = ./shell.nix;
-        };
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in
+            {
+                packages = rec {
+                    default = txtx;
+                    txtx = pkgs.callPackage ./. {};
+                };
+                apps = rec {
+                    default = txtx;
+                    txtx = flake-utils.lib.mkApp { drv = self.packages.${system}.txtx; };
+                };
+                devShell = import ./shell.nix { inherit pkgs; };
+            }
+        );
 }
